9bc7577dd1d71c0b71db16efaae5c24cd3785380
Jooyul Yoon <juyeolyoon@gmail.com>
2023-12-04 15:23:36 -0800

    django rest framework setup

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from rest_framework.response import Response
+from .models import Member
+from rest_framework.views import APIView
+from .serializers import MemberSerializer
+class MemberListAPI(APIView):
+    def get(self, request):
+        queryset = Member.objects.all()
+        print(queryset)
+        serializer = MemberSerializer(queryset, many=True)
+        return Response(serializer.data)
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api.views import MemberListAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('api/member/', MemberListAPI.as_view())
 ]
