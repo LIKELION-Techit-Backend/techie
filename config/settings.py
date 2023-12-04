0b2e65d6a2e81645681afb230af428a0aca6ef30
Jooyul Yoon <juyeolyoon@gmail.com>
2023-12-04 13:49:14 -0800

    db setup

--- config/settings.py
+++ config/settings.py
@@ -75,11 +75,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Schema1',
+        'USER': 'admin',
+        'PASSWORD': '12345678',
+        'HOST': 'techit-dashboard.c1puvxyz3nuy.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8',
+            'use_unicode': True,
+        },
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +113,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+USE_TZ = True
+TIME_ZONE = 'US/Pacific'
 
 USE_I18N = True
 
