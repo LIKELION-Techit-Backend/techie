0bae61e8b2f0ecb54ec468790faeea73c1acffc0
Jooyul Yoon <juyeolyoon@gmail.com>
2023-12-04 15:23:10 -0800

    feat: create table user, team

--- api/models.py
+++ api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class Member(models.Model):    
+  id = models.BigAutoField(primary_key=True)
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  email = models.CharField(max_length=30)
+  password = models.CharField(max_length=20)
+  
+class Team(models.Model):
+  id = models.BigAutoField(primary_key=True)
+  team_name = models.CharField(max_length=30)
