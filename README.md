4762a2e49f472f33cdf17d9549e78507ab571df5
yhk338 <yanghwi42@gmail.com>
2024-01-13 04:15:10 -0800

    Add deploy files, Update README for deploy to AWS Beanstalk

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # techie
+
+### How to deploy to AWS Beanstalk
+
+1. Zip the current repo using the following command. Below command ensures the hidden `.ebextensions` folder gets included.
+    ```bash
+    zip -r ../techit.zip \.* *
+    ```
+2. Go to `config/settings.py` and set `DEBUG` to `False`.
+3. Go visit AWS beanstalk and just upload the zip file and deploy.
--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lj8-!l8&w*hn+$nmlc5h7h1x055!y$2=j)p14p%grv8j*4vdtf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["Techit-env.eba-wdccmhiz.us-east-2.elasticbeanstalk.com"]
 
 
 # Application definition
