c8c34d19f12b74c428d74fff0705cee90de6160e
Jooyul Yoon <juyeolyoon@gmail.com>
2024-02-03 18:04:27 -0800

    autopep

--- api/views.py
+++ api/views.py
@@ -1,10 +1,9 @@
-
-from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Member, Lecture,Course, Team, Taken
+from .models import Member, Lecture, Course, Team, Taken
 from rest_framework.views import APIView
 from .serializers import MemberSerializer, TeamSerializer, LectureSerializer, CourseSerializer, TakenSerializer
+
 
 class MemberAPI(APIView):
     def get(self, request, id):
@@ -13,48 +12,49 @@ class MemberAPI(APIView):
             serializer = MemberSerializer(member)
             return Response(serializer.data)
         except:
-            return Response({"message": "member not found"},status=status.HTTP_404_NOT_FOUND)       
-        
+            return Response({"message": "member not found"}, status=status.HTTP_404_NOT_FOUND)
+
     def put(self, request, id):
         try:
             query = Member.objects.get(id=id)
         except Member.DoesNotExist:
-            return Response({'error' : {'message' : "member not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
-        member = MemberSerializer(query,data=request.data, partial=True)
+            return Response({'error': {'message': "member not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
+        member = MemberSerializer(query, data=request.data, partial=True)
         if member.is_valid():
             member.save()
             return Response(member.data)
-        return Response(member.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(member.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         try:
             query = Member.objects.get(id=id)
         except Member.DoesNotExist:
-            return Response({'error' : {'message' : "member not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
+            return Response({'error': {'message': "member not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
         result = Member.objects.get(id=id)
         result.delete()
         return Response({"message": "successfully deleted!"}, status=204)
-    
+
+
 class MemberListAPI(APIView):
     def get(self, request):
         queryset = Member.objects.all()
         serializer = MemberSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         member = MemberSerializer(data=request.data)
-        
+
         if member.is_valid():
             if len(Member.objects.filter(email=request.data['email'])) == 0:
                 member.save()
                 return Response(member.data)
             else:
-                return Response({"message": "email already exists"},status=status.HTTP_409_CONFLICT)        
-        return Response(member.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    
+                return Response({"message": "email already exists"}, status=status.HTTP_409_CONFLICT)
+        return Response(member.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
 class TeamAPI(APIView):
     def get(self, request, id):
         try:
@@ -62,15 +62,15 @@ class TeamAPI(APIView):
             serializer = TeamSerializer(team)
             return Response(serializer.data)
         except:
-            return Response({"message": "team not found"},status=status.HTTP_404_NOT_FOUND)       
-        
+            return Response({"message": "team not found"}, status=status.HTTP_404_NOT_FOUND)
+
     def put(self, request, id):
         try:
             query = Team.objects.get(id=id)
         except Team.DoesNotExist:
-            return Response({'error' : {'message' : "team not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
-        team = TeamSerializer(query,data=request.data, partial=True)
+            return Response({'error': {'message': "team not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
+        team = TeamSerializer(query, data=request.data, partial=True)
         if team.is_valid():
             team.save()
             return Response(team.data)
@@ -80,28 +80,30 @@ class TeamAPI(APIView):
         try:
             query = Team.objects.get(id=id)
         except Team.DoesNotExist:
-            return Response({'error' : {'message' : "team not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
+            return Response({'error': {'message': "team not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
         result = Team.objects.get(id=id)
         result.delete()
         return Response({"message": "successfully deleted!"}, status=204)
+
 
 class TeamListAPI(APIView):
     def get(self, request):
         queryset = Team.objects.all()
         serializer = TeamSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         team = TeamSerializer(data=request.data)
         if team.is_valid():
             try:
                 Team.objects.get(team_name=request.data['team_name'])
-                return Response({"message": "team already exists"},status=status.HTTP_409_CONFLICT)        
+                return Response({"message": "team already exists"}, status=status.HTTP_409_CONFLICT)
             except:
                 team.save()
                 return Response(team.data)
-        return Response(team.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(team.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LectureListAPI(APIView):
     def get(self, request):
@@ -109,19 +111,20 @@ class LectureListAPI(APIView):
         print(queryset)
         serializer = LectureSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         print(request.data)
         print("============================")
         lecture = LectureSerializer(data=request.data)
-        
+
         if lecture.is_valid():
             if len(Lecture.objects.filter(lecture_name=request.data['lecture_name'])) == 0:
                 lecture.save()
                 return Response(lecture.data)
             else:
-                return Response({"message": "lecture id already exists"},status=status.HTTP_409_CONFLICT)        
+                return Response({"message": "lecture id already exists"}, status=status.HTTP_409_CONFLICT)
         return Response(lecture.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class LectureAPI(APIView):
     def get(self, request, id):
@@ -130,24 +133,25 @@ class LectureAPI(APIView):
             serializer = LectureSerializer(lecture)
             return Response(serializer.data)
         except:
-            return Response({"message": "lecture not found"},status=status.HTTP_404_NOT_FOUND)       
-        
+            return Response({"message": "lecture not found"}, status=status.HTTP_404_NOT_FOUND)
+
     def put(self, request, id):
         try:
             query = Lecture.objects.get(id=id)
         except Lecture.DoesNotExist:
-            return Response({'error' : {'message' : "lecture not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
-        lecture = LectureSerializer(query,data=request.data)
+            return Response({'error': {'message': "lecture not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
+        lecture = LectureSerializer(query, data=request.data)
         if lecture.is_valid():
             lecture.save()
             return Response(lecture.data)
-        return Response(lecture.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(lecture.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         result = Lecture.objects.get(id=id)
         result.delete()
-        return Response({"message" : "successfully deleted"}, status=204)
+        return Response({"message": "successfully deleted"}, status=204)
+
 
 class CourseAPI(APIView):
     def get(self, request, id):
@@ -156,45 +160,47 @@ class CourseAPI(APIView):
             serializer = CourseSerializer(course)
             return Response(serializer.data)
         except:
-            return Response({"message": "course not found"},status=status.HTTP_404_NOT_FOUND)       
-        
+            return Response({"message": "course not found"}, status=status.HTTP_404_NOT_FOUND)
+
     def put(self, request, id):
         try:
             query = Course.objects.get(id=id)
         except Course.DoesNotExist:
-            return Response({'error' : {'message' : "course not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
-        course = CourseSerializer(query,data=request.data)
+            return Response({'error': {'message': "course not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
+        course = CourseSerializer(query, data=request.data)
         if course.is_valid():
             course.save()
             return Response(course.data)
-        return Response(course.errors,status=status.HTTP_400_BAD_REQUEST)
+        return Response(course.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, id):
         try:
             query = Course.objects.get(id=id)
         except Course.DoesNotExist:
-            return Response({'error' : {'message' : "course not found!"}}, status = status.HTTP_404_NOT_FOUND)
+            return Response({'error': {'message': "course not found!"}}, status=status.HTTP_404_NOT_FOUND)
         result = Course.objects.get(id=id)
         result.delete()
         return Response({'message': 'successfully deleted!'}, status=204)
+
 
 class CourseListAPI(APIView):
     def get(self, request):
         queryset = Course.objects.all()
         serializer = CourseSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         course = CourseSerializer(data=request.data)
-        
+
         if course.is_valid():
             if len(Course.objects.filter(title=request.data['title'])) == 0:
                 course.save()
                 return Response(course.data)
             else:
-                return Response({"message": "course already exists"},status=status.HTTP_409_CONFLICT)        
-        return Response(course.errors,status=status.HTTP_400_BAD_REQUEST)
+                return Response({"message": "course already exists"}, status=status.HTTP_409_CONFLICT)
+        return Response(course.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class TakenAPI(APIView):
     def get(self, request, id):
@@ -203,18 +209,19 @@ class TakenAPI(APIView):
             serializer = TakenSerializer(taken)
             return Response(serializer.data)
         except:
-            return Response({"message": "taken course is not found"},status=status.HTTP_404_NOT_FOUND)
+            return Response({"message": "taken course is not found"}, status=status.HTTP_404_NOT_FOUND)
 
     def delete(self, request, id):
         try:
             Taken.objects.get(id=id)
         except Taken.DoesNotExist:
-            return Response({'error' : {'message' : "taken not found!"}}, status = status.HTTP_404_NOT_FOUND)
-        
+            return Response({'error': {'message': "taken not found!"}}, status=status.HTTP_404_NOT_FOUND)
+
         result = Taken.objects.get(id=id)
         result.delete()
         return Response({"message": "successfully deleted!"}, status=status.HTTP_200_OK)
-    
+
+
 class TakenListAPI(APIView):
     def get(self, request):
         mid = request.GET.get('mid')
@@ -224,26 +231,26 @@ class TakenListAPI(APIView):
             queryset = Taken.objects.all()
         serializer = TakenSerializer(queryset, many=True)
         return Response(serializer.data)
-    
+
     def post(self, request):
         mid = request.data['mid']
         lid = request.data['lid']
-        
+
         # check member
         try:
             member = Member.objects.get(id=mid)
         except Member.DoesNotExist:
-            return Response({"message": "member does not exists"},status=status.HTTP_404_NOT_FOUND)        
+            return Response({"message": "member does not exists"}, status=status.HTTP_404_NOT_FOUND)
         # check lecture
         try:
             lecture = Lecture.objects.get(id=lid)
         except Lecture.DoesNotExist:
-            return Response({"message": "lecture does not exists"},status=status.HTTP_404_NOT_FOUND)        
+            return Response({"message": "lecture does not exists"}, status=status.HTTP_404_NOT_FOUND)
         # check unique
         try:
             taken = Taken.objects.create(member=member, lecture=lecture)
         except:
-            return Response({"message": "already taken"},status=status.HTTP_409_CONFLICT)
+            return Response({"message": "already taken"}, status=status.HTTP_409_CONFLICT)
         # save
         try:
             serializer = TakenSerializer(taken)
@@ -262,15 +269,15 @@ class SyncAPI(APIView):
             return member_serialized
         except Member.DoesNotExist as e:
             return None
-        
+
     def _get_lectures(self, course_serialized):
         try:
             course_id = course_serialized.data.get("id")
             lectures = Lecture.objects.filter(course_id=course_id)
-            return lectures  
+            return lectures
         except:
             return None
-        
+
     def _get_course(self, title, code):
         try:
             course = Course.objects.get(title=title, code=code)
@@ -281,9 +288,9 @@ class SyncAPI(APIView):
 
     def post(self, request):
         try:
-            member = self._get_member(request=request) 
+            member = self._get_member(request=request)
             if member is None:
-                return Response({"message": "Member does not exist in DB"}, status=status.HTTP_404_NOT_FOUND)                  
+                return Response({"message": "Member does not exist in DB"}, status=status.HTTP_404_NOT_FOUND)
             title = request.data["title"]
             code = request.data["code"]
             lectures = request.data["course"]
@@ -293,11 +300,14 @@ class SyncAPI(APIView):
                 course_serialized = CourseSerializer(data=data)
                 if course_serialized.is_valid():
                     course_serialized.save()
-                lecturelist = [{"lecture_name": l.get("title"), "course_id": course_serialized.data.get("id")} for l in lectures]
-                lecture_serialized = LectureSerializer(data=lecturelist, many=True)
+                lecturelist = [{"lecture_name": l.get(
+                    "title"), "course_id": course_serialized.data.get("id")} for l in lectures]
+                lecture_serialized = LectureSerializer(
+                    data=lecturelist, many=True)
                 if lecture_serialized.is_valid():
                     lecture_serialized.save()
-            lecture_serialized = self._get_lectures(course_serialized=course_serialized)
+            lecture_serialized = self._get_lectures(
+                course_serialized=course_serialized)
             print(lecture_serialized)
             # taken = []
             # member_id = member.data.get("id")
@@ -310,7 +320,6 @@ class SyncAPI(APIView):
             # if taken_serialized.is_valid():
             #     print(f"serializer ===== {taken_serialized.data}")
             #     taken_serialized.save()
-            return Response(status=status.HTTP_200_OK)             
+            return Response(status=status.HTTP_200_OK)
         except Exception as e:
-            return Response({"message": f"{e}"},status=status.HTTP_404_NOT_FOUND)       
-
+            return Response({"message": f"{e}"}, status=status.HTTP_404_NOT_FOUND)
