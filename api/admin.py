a8e555e618591ee69d61bcb8b131143cf8e6fb49
Jooyul Yoon <juyeolyoon@gmail.com>
2024-02-17 13:36:04 -0800

    admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from api.models import Course, Lecture, Member, Pending, Taken, Team
 
-# Register your models here.
+admin.site.register(Member)
+admin.site.register(Team)
+admin.site.register(Taken)
+admin.site.register(Lecture)
+admin.site.register(Course)
+admin.site.register(Pending)
